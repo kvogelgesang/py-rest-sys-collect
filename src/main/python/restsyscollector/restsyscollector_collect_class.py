e8f4aa94c367abff3726ef9b200f424b89839f5b
kvogelgesang <kay .vogelgesang@apachefriends.org>
2016-08-17 22:28:15 +0200

    In progress ... (1)

--- src/main/python/restsyscollector/restsyscollector_collect_class.py
+++ src/main/python/restsyscollector/restsyscollector_collect_class.py
@@ -3,3 +3,21 @@
 import sys
 sys.path.append("../python")
 sys.path.append("src/main/python")
+
+class RestSysCollectorCollect():
+
+    def getFileDataAsList(self, fullfilename):
+        try:
+            f = open(fullfilename, 'r')
+            return f.readlines()
+        except IOError as e:
+            print("I/O error({0}): {1}".format(e.errno, e.strerror))
+            raise
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+            raise
+        return False
+
+
+if __name__ == '__main__':
+    pass
